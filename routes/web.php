071824b6e433324d3b1368aae66536e9472829f9
long <396667187@qq.com>
2019-02-28 10:56:40 +0000

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,9 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
         // 收藏商品列表
         Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
+        // 加入购物车
+        Route::post('cart', 'CartController@add')->name('cart.add');
     });
 
 });
