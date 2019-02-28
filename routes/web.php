4042a5a653102501313e2498f019770f24f413b9
long <396667187@qq.com>
2019-02-28 10:19:07 +0000

    用户界面 - 商品收藏

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,11 @@ Route::group(['middleware' => 'auth'], function() {
 
         // 删除地址
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+
+        // 收藏商品
+        Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+        // 取消收藏
+        Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     });
 
 });
