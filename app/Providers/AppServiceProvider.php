01d23b688745ae00e57e649cd4da7c8f19f78315
long <396667187@qq.com>
2019-03-01 02:53:27 +0000

    订单支付宝支付流程

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,12 @@ class AppServiceProvider extends ServiceProvider
     {
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
+
             $config = config('pay.alipay');
+            // $config['notify_url'] = route('payment.alipay.notify'); // 后端回调
+            $config['notify_url'] = 'https://requestbin.leo108.com/17c9he91';
+            $config['return_url'] = route('payment.alipay.return'); // 前端回调
+
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
                 $config['mode']         = 'dev';
--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,11 @@ Route::group(['middleware' => 'auth'], function() {
 
         // 订单详情
         Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+
+        // 支付宝订单支付
+        Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+        // 支付宝前端回调
+        Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     });
 
 });
@@ -75,10 +80,13 @@ Route::group(['middleware' => 'auth'], function() {
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
-Route::get('alipay', function() {
-    return app('alipay')->web([
-        'out_trade_no' => time(),
-        'total_amount' => '1',
-        'subject' => 'test subject - 测试',
-    ]);
-});
+// Route::get('alipay', function() {
+//     return app('alipay')->web([
+//         'out_trade_no' => time(),
+//         'total_amount' => '1',
+//         'subject' => 'test subject - 测试',
+//     ]);
+// });
+
+// 支付宝服务器端回调
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
