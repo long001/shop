7dfd3c43058db5e49e55073ef91750557a952c79
long <396667187@qq.com>
2019-03-01 09:08:21 +0000

    用户界面 - 申请退款

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,9 @@ Route::group(['middleware' => 'auth'], function() {
 
         Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
+
+        // 退款申请
+        Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
     });
 
 });
